728ace82876fd31d2bbc9087aaaf4cbd5a6b8b3a
darrius <mohamedtine17@gmail.com>
2023-09-03 17:29:27 +0000

    Add next js

---
+++ app/app/layout.tsx
@@ -0,0 +1,29 @@
+"use client";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { createConfig, mainnet, WagmiConfig } from "wagmi";
+import { http, createPublicClient } from "viem";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const config = createConfig({
+  publicClient: createPublicClient({
+    chain: mainnet,
+    transport: http("127.0.0.1:8545"),
+  }),
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <WagmiConfig config={config}>{children}</WagmiConfig>
+      </body>
+    </html>
+  );
+}
---
+++ app/app/page.tsx
@@ -0,0 +1,9 @@
+import Wallet from "@/components/wallet";
+
+export default function Home() {
+  return (
+    <main>
+        <Wallet/>
+    </main>
+  );
+}
